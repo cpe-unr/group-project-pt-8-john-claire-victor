c65d0d9f4f576b56ce2e36ce336d7deebdf69b7c
vpercival <70241187+vpercival@users.noreply.github.com>
2021-05-03 19:57:02 -0700

    Updated files with comments

--- Audio Processor Normalization/Processor.h
+++ Audio Processor Normalization/Processor.h
@@ -1,9 +1,23 @@
+/*
+  ===================================================================
+  Project Name: Semester Project
+  Author: Victor A. Percival
+  Date: 05/03/21
+  Filename: Processor.h
+  ===================================================================
+*/
+
+
 #ifndef CLASS_PROCESSOR_H
 #define CLASS_PROCESSOR_H
 
 class Processor
 {
 private:
+	/**
+	* These values will be used to assist in the calculation of
+	* normalization.
+	*/
 	float peakAmplitude;
 	float newAmplitude;
 	float newMultiplier;
@@ -11,6 +25,10 @@ public:
 	Processor(void);
 	~Processor(void);
 
+	/**
+	 * These functions are used to find the peak amplitude and 
+	 * normalize the audio of the audio file.
+	 */
 	float findAmplitude(float peakAmplitude);
 	const float multiplier(float newAmplitude / float peakAmplitude);
 };
