d4a6747a40e4a563019d9ec9d1f3f638d89a66bf
vpercival <70241187+vpercival@users.noreply.github.com>
2021-05-03 19:56:01 -0700

    Updated files with comments

--- User Interface/UserInterface.cpp
+++ User Interface/UserInterface.cpp
@@ -20,28 +20,45 @@ UserInterface::~UserInterface(void)
 {
 }
 
+ /**
+  * This is the getter for first main menu user selection.
+  */
 int UserInterface::getUserSelection(void)
 {
 	return userSelection;
 }
 
+ /**
+  * This is the setter for first main menu user selection.
+  */
 void UserInterface::setUserSelection(int newSel)
 {
 	userSelection = newSel;
 	return;
 }
 
+ /**
+  * This is the getter that will be used for the proceeding menu.
+  */
 int UserInterface::getUserSelection2(void)
 {
 	return userSelection2;
 }
 
+ /**
+  * This is the setter that will be used for the proceeding menu.
+  */
 void UserInterface::setUserSelection2(int newSel2)
 {
 	userSelection2 = newSel2;
 	return;
 }
 
+
+ /**
+  * This is the main menu that is displayed to the user upon running the program.
+  * The first two selections will display another menu that asks the user for an audio format.
+  */
 void UserInterface::displayMenu(void)
 {
 	std::cout << "***  AUDIO MENU  ***" << std::endl;
@@ -53,6 +70,11 @@ void UserInterface::displayMenu(void)
 	std::cout << "0. EXIT PROGRAM " << std::endl << std::endl; 
 	return;
 }
+
+ /** 
+  * If the user does not select an option between 0 and 4, an error message will be displayed and the user will be asked to make a correct selection.
+  * This will loop until the user makes a correct selection.
+  */
 
 int UserInterface::mainSelection(int userSelection)
 {
@@ -75,6 +97,12 @@ int UserInterface::mainSelection(int userSelection)
 	return userSelection;
 }
 
+ /**
+  * This is the displayed menu for a secondary selection unpon selecting one of the first two choices.
+  * Depending on which of the two selections the user made, each of these will perform that analysis for the specified audio type.
+  * The first two selections will display another menu that asks the user for an audio format.
+  */
+
 void UserInterface::displayTypesMenu(void)
 {
 	system("CLS");
@@ -91,6 +119,12 @@ void UserInterface::displayTypesMenu(void)
 	std::cout << "0. EXIT PROGRAM " << std::endl << std::endl;
 	return;
 }
+
+/** 
+  * This is similar to the logic for ensuring the user makes a valid selection in the main menu.
+  * If the user does not select an option between 0 and 8, an error message will be displayed and the user will be asked to make a correct selection.
+  * This will loop until the user makes a correct selection.
+  */
 
 int UserInterface::typeSelection(int userSelection2)
 {
